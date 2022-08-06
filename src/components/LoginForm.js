b53318f72769d458b952217528ae15f707125db8
leandrotedini <leandro.tedini@gmail.com>
2022-08-06 16:08:37 -0300

    first commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
+  return <>
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </>
+}
+
+export default LoginForm
